7f242e3735787203145dec79b0c1636ce3709be0
Tomaz Chevres <tchevres-24@peddie.org>
2023-03-08 10:21:08 -0500

    nodejs

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,3 +1,16 @@
+// this should be at the top of app.js
 const express = require('express');
-
+const app = express();
 const port = 5623;
+
+
+// sets express to listen on local port 5622
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+
+// test to make sure it is working
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
