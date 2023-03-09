eaaa43816be7e3f9ad731005b1977705824187d2
Tomaz Chevres <tchevres-24@peddie.org>
2023-03-08 23:40:37 -0500

    set root directory

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,14 +1,29 @@
-// this should be at the top of app.js
+//stuff to connect to app.ks
 const express = require('express');
 const app = express();
 const port = 5623;
+
+const fs = require('fs');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+
+// Read in the contents of the secure.json file
+var secureData = fs.readFileSync('secure.json');
+const secure = JSON.parse(secureData);
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : secure.clientId,
+  clientSecret : secure.clientSecret,
+});
+
+console.log(spotifyApi);
+
 
 
 // sets express to listen on local port 5623
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
 
 // test to make sure it is working
 app.get('/', (req, res) => {
