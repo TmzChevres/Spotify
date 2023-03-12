85f248774623f7612caafe29e9f4d831c0a5030d
Tomaz Chevres <tchevres-24@peddie.org>
2023-03-12 00:46:55 -0500

    get artists

--- web.js
+++ web.js
@@ -7,9 +7,20 @@ if (searchParams.get("error") == "access_denied") {
     console.log("Error: access_denied")
     window.location = "index.html"
 }
-
-
 const token = searchParams.get("access_token");
+
+//read artists & update list
+var artists;
+
+getTopArtists("medium_term", 10)
+  .then(data => {
+    for(var artist in data){
+        document.getElementsByClassName("artist-list")[0].appendChild("<li></li>")
+    }
+  })
+  .catch(error => console.error(error));
+
+
 
 //return the user's top artists,default: time_range="medium-term" limit=10
 //https://developer.spotify.com/documentation/web-api/reference/#/operations/get-users-top-artists-and-tracks
@@ -26,8 +37,3 @@ function getTopArtists(time_range, limit) {
         .then(data => data.items)
         .catch(error => console.error(error));
 }
-
-console.log(token);
-getTopArtists("medium_term", 10)
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
