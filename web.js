888c5c8cb4f083a71a0fa4f7d8113a13842b500a
Tomaz Chevres <tchevres-24@peddie.org>
2023-03-12 00:47:55 -0500

    get artists

--- web.js
+++ web.js
@@ -15,7 +15,7 @@ var artists;
 getTopArtists("medium_term", 10)
   .then(data => {
     for(var artist in data){
-        document.getElementsByClassName("artist-list")[0].appendChild("<li></li>")
+        document.getElementsByClassName("artist-list")[0].innerHTML+='<li class="artist-item"></li>';
     }
   })
   .catch(error => console.error(error));
